c3397a25b030939f574370a0455dfa9c9135d4e6
bapon <bapon@umail.ucsb.edu>
2018-03-21 23:28:22 -0700

    changed name

--- mux_padding_test.py
+++ mux_padding_test.py
@@ -39,69 +39,58 @@ def barrel_shifter_v2(bits_to_shift, bit_in, direction, shift_dist, wrap_around=
 
     return val
 
-def mux_pooling(start, vecs, vecs_length, nvecs, matrix_size, mat_size_reg):
+def mux_padding(start, vecs, vecs_length, nvecs, matrix_size):
+	#need to check to make sure values are stored on which end of the list
 	#NOTE: ONLY SUPPORTS 2 INPUTS. MAXPOOL AND PASSED IN VALUE. needs second layer of muxes for additional features
 	busy = Register(1)
 	done = Register(1)
-	init = Register(1)
-	constant_val = Register(32)
+	constant_val = Const(2147483648) #0x80000000
 	counter = Register(int(math.log(matrix_size,2))+1)
-	mask = Register(matrix_size)
+	mask = Const(int(math.pow(2, matrix_size)-1)) #0xfff..f
 	mux_control = Register(matrix_size)
 	output_vecs = [Register(32) for i in range(0,matrix_size)]
 	with conditional_assignment:
 		with start:
-			busy.next |= 0
 			counter.next |= 0
-			init.next |= 1
-			done.next |= 0
-			constant_val.next |= 0x80000000
-			mask.next |= barrel_shifter_v2(mask, 1, 0, mat_size_reg)
-		with init:
+			done.next |= 0	
 			busy.next |= 1
-			init.next |= 0
-			mux_control.next |= barrel_shifter_v2(mask, 0, 0, vecs_length)
+			mux_control.next |= barrel_shifter_v2(mask, 0, 1, vecs_length)
 		with busy:
 			counter.next |= counter + 1
 			with counter == matrix_size:
 				busy.next |= 0
 				done.next |= 1
-			with counter >= nvecs: #checks to see if all values have been passed in. 
+			with counter >= nvecs-1: #checks to see if all values have been passed in. 
 				mux_control.next |= mask
 			with otherwise: 
-				mux_control.next |= barrel_shifter_v2(mask, 0, 0, vecs_length)
+				mux_control.next |= barrel_shifter_v2(mask, 0, 1, vecs_length)
 			for i in range(0, matrix_size):
 				output_vecs[i].next |= mux(mux_control[i], vecs[i], constant_val)
-	return output_vecs, mux_control, counter, busy, init, done, mask
+	return output_vecs, mux_control, counter, busy, done, mask
 
 reg_vec = [pyrtl.Register(32, 'reg_{}'.format(i)) for i in range(0, 8)]
 inputs = [pyrtl.Input(32, 'input_{}'.format(i)) for i in range(0, 8)]
 start = pyrtl.Input(1, 'start')	
-nvecs = pyrtl.Register(4, 'nvecs')
-vecs_length = Register(4, 'vecs_length')
-mat_size_reg = Register(4, 'mat_size_reg')
+nvecs = pyrtl.Register(8, 'nvecs')
+vecs_length = Const(4)
 nvecs.next <<= 6
 mat_size = 8
-mat_size_reg.next <<= 8
-pool_size = 2
-vecs_length.next <<= 6
-shift_amt = 3
 test_dict = {
-        'input_0': 4, 
-        'input_1': 4, 
-        'input_2': 4, 
-        'input_3': 4, 
-        'input_4': 4, 
-        'input_5': 4, 
-        'input_6': 4, 
-        'input_7': 4,
-        'start': 1
-        }
+		'input_0': 4, 
+		'input_1': 4, 
+		'input_2': 4, 
+		'input_3': 4, 
+		'input_4': 4, 
+		'input_5': 4, 
+		'input_6': 4, 
+		'input_7': 4,
+		'start': 1
+		}
 # output_val = Output(int(math.log(vecs_length, 2))+1, 'output_amt')
-output_vecs, mux_control, counter, busy, init, done, mask = mux_pooling(start, reg_vec, vecs_length, nvecs, mat_size, mat_size_reg)
+output_vecs, mux_control, counter, busy, done, mask = mux_padding(start, reg_vec, vecs_length, nvecs, mat_size)
 
 for index,reg in enumerate(reg_vec):
-    reg.next <<= inputs[index]
+	reg.next <<= inputs[index]
 # output_val <<= output_amt
 
 for index,reg in enumerate(output_vecs):
@@ -111,26 +100,26 @@ probe(counter, 'counter')
 probe(done, 'done')
 probe(busy, 'busy')
 probe(mask, 'mask')
-probe(init, 'init')
+# probe(vecs_length, 'vecs_length')
 
 sim_trace = pyrtl.SimulationTrace()
 sim = pyrtl.Simulation(tracer=sim_trace)
 
 for cycle in range(30):
-    sim.step(test_dict)
-    if(cycle >= 0):
-        test_dict = {
-            'input_0': 4, 
-            'input_1': 4, 
-            'input_2': 4, 
-            'input_3': 4, 
-            'input_4': 4, 
-            'input_5': 4, 
-            'input_6': 4, 
-            'input_7': 4,
-            'start': 0  
-            }
+	sim.step(test_dict)
+	if(cycle >= 0):
+		test_dict = {
+			'input_0': 4, 
+			'input_1': 4, 
+			'input_2': 4, 
+			'input_3': 4, 
+			'input_4': 4, 
+			'input_5': 4, 
+			'input_6': 4, 
+			'input_7': 4,
+			'start': 0  
+			}
 # Now all we need to do is print the trace results to the screen. Here we use
 # "render_trace" with some size information.
 print('--- Simulation ---')
-sim_trace.render_trace(symbol_len=12, segment_size=35)
+sim_trace.render_trace(symbol_len=12, segment_size=5)
